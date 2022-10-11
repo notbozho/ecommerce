8b15ae77212bf043c0b701c07ec49e984160e1f4
notbozho <bozhoivanow@gmail.com>
2022-10-11 15:46:16 +0300

    add eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
 
-  return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>hi</h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
