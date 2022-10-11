a0cb5a686b3d0f0d0b50451afe050464cef12324
notbozho <bozhoivanow@gmail.com>
2022-10-11 21:12:09 +0300

    add random footer

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { chakra } from "@chakra-ui/react";
+
+const Logo = () => {
+    return (
+        <div>
+            <chakra.svg>
+                {/* add svg logo here */}
+            </chakra.svg>
+        </div>
+    );
+};
+
+export default Logo;
