a0300d5c61208693c18fdcb09b0cb306d365aa83
Radoslav Radev <88380154+calisthenicsGuy@users.noreply.github.com>
2022-10-13 04:30:31 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import { Route, Routes } from "react-router-dom";
 import "./App.less";
 
 // components
-import Header from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 // pages
 import Home from "./pages/Home";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={Home} />
-        <Route path="/home" element={Home} />
-      </Routes>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={Home} />
+                <Route path="/home" element={Home} />
+            </Routes>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 function Navbar() {
+<<<<<<< Updated upstream
   return (
     <div className="navbar"></div>
   );
+=======
+    return (
+        <div className="navbar">
+        </div>
+    );
+>>>>>>> Stashed changes
 }
 
 export default Navbar;
